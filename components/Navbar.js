d52d90faeacfa889e64b43b0f29dfc961b1793c0
Madhurish Katta <33370824+madhurish@users.noreply.github.com>
2025-08-26 21:22:23 +0530

    Revert "Initial commit: Next.js Wishcoin project setup"
    This reverts commit 1b6c46b09b62399c9e8777451203bda1fafb4581.

--- components/Navbar.js
+++ components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar() {
     <nav className={`${styles.navbar} ${isScrolled ? styles.scrolled : ''}`}>
       <div className={styles.container}>
         <div className={styles.logo}>
-          <span>Wishcoin</span>
+          <span>Agency</span>
         </div>
         
         <div className={`${styles.navLinks} ${isMobileMenuOpen ? styles.mobileOpen : ''}`}>
