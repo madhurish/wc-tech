b5232cb9bffd41faf285d8b14fb5a91fc42abec3
Madhurish Katta <33370824+madhurish@users.noreply.github.com>
2025-09-04 17:26:10 +0530

    Adding google tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
 import '../styles/globals.css'
 import '../styles/fonts.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V6DDTJT3J8"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-V6DDTJT3J8');
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
